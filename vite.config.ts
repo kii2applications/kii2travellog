334ca09c6dff10406b8f19b573e56464bef31047
kii2applications <74774919+kii2applications@users.noreply.github.com>
2025-06-08 17:52:06 +0400

    updated manifest

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/kii2travellog/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
